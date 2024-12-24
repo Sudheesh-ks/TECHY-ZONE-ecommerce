f6c0439ac0fe13c34b0cdf7d96b123e153d983d7
Sudheesh ks <sudheeshks132@gmail.com>
2024-12-24 12:04:32 +0530

    update

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use('/admin',adminRoute);
 
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
     
 })
