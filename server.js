f92e56e99244caf13fcf08799deb84a2901889da
Sudheesh ks <sudheeshks132@gmail.com>
2024-12-24 11:07:20 +0530

    feat: small update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config(); 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 const express = require('express');
 const session = require('express-session');
