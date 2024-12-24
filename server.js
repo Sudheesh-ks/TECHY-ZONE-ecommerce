a5c9cf3c1869b434568a07bfde6354f7c292616c
Sudheesh ks <sudheeshks132@gmail.com>
2024-12-24 11:52:54 +0530

    feat: configured the port on .env

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use('/admin',adminRoute);
 
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
     
 })
