c0f3562496bdfc2a1ab1a04996e547479f114571
Sudheesh ks <sudheeshks132@gmail.com>
2026-04-21 16:03:03 +0530

    fix: added callback url to .env

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:4001/auth/google/callback' 
+    callbackURL: process.env.CALLBACK_URL 
 },
 async (accessToken, refreshToken, profile, done) => {
     console.log("Google Profile:", profile); 
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -76,8 +76,5 @@ router.get('/addCoupon',couponController.loadAddCoupon);
 router.post('/add-coupon', couponController.addCoupon);
 router.delete('/delete-coupon/:id', couponController.deleteCoupon);
 
-router.get('/editCoupon/:id', couponController.loadEditCoupon);
-router.put('/edit-coupon/:id', couponController.updateCoupon);
-
 
 module.exports = router;
